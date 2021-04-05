6f16bb7cdf0db74cea0f51952d8d2515884ef631
Ricky Hartmann <rhartmann3@gmail.com>
2021-04-05 09:35:27 -0400

    add remove from localStorage option

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ function putInLocalStorage() {
   localStorage.setItem('nohs:openExternal', 'https://dev1userfileupload.blob.core.windows.net/filetest1/3ipok4_20200901091408354.jpg?sp=r&st=2021-04-05T13:17:25Z&se=2021-04-05T21:17:25Z&spr=https&sv=2020-02-10&sr=b&sig=x4Kw20FyRsxrxT4Gz4D0x5xSihnsPzGVhUXqbfYNAdI%3D');
 }
 
+function removeFromLocalStorage() {
+  localStorage.removeItem('nohs:openExternal');
+}
+
 function postAndLog(data) {
   if (window && window.parent) {
     window.parent.postMessage(data, '*');
